ed601cf54e6655fe3987a989a4cd78a6b74ebd7d
coderrim <hyorim2102@gmail.com>
2025-11-09 12:43:43 +0900

    보안성 검토 백엔드 동작 완료 & 보안성 검토 파일 정리

--- epiaa/src/main/java/com/epia/repo/SecurityTargetRepo.java
+++
@@ -1,9 +0,0 @@
-//package com.epia.repo;
-//
-//import com.epia.domain.SecurityTarget;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import java.util.List;
-//
-//public interface SecurityTargetRepo extends MongoRepository<SecurityTarget, Integer> {
-//    List<SecurityTarget> findByCompanyId(String companyId);
-//}
