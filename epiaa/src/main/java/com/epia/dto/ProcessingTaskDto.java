6e3a6198b3827510fdee5cd0351be411b3224c2e
coderrim <hyorim2102@gmail.com>
2025-11-12 12:40:09 +0900

    처리업무표 완료

--- epiaa/src/main/java/com/epia/dto/ProcessingTaskDto.java
+++ epiaa/src/main/java/com/epia/dto/ProcessingTaskDto.java
@@ -3,11 +3,25 @@ package com.epia.dto;
 public class ProcessingTaskDto {
     public String id;
     public String taskName;
+    public String purpose;
+    public String department;
+    public String infomation;
+    public String companyId;
 
     public ProcessingTaskDto() {}
 
     public ProcessingTaskDto(String id, String taskName) {
         this.id = id;
         this.taskName = taskName;
+    }
+
+    public ProcessingTaskDto(String id, String taskName, String purpose,
+                             String department, String infomation, String companyId) {
+        this.id = id;
+        this.taskName = taskName;
+        this.purpose = purpose;
+        this.department = department;
+        this.infomation = infomation;
+        this.companyId = companyId;
     }
 }
