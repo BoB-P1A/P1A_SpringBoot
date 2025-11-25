7e25cfe38200f84194fef7ac6d335a94402f48d7
deepserver <ckdgusman@gmail.com>
2025-11-25 19:40:06 +0900

    https 호환

--- P1A_back/src/main/java/com/epia/config/WebConfig.java
+++ P1A_back/src/main/java/com/epia/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins(
+                            "http://localhost:8080",
+                            "https://epia.store",
+                            "https://www.epia.store"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
